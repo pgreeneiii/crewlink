c28ab8946623247e5566663d2fffd40181efb1b9
Phil Greene <pgreeneiii@users.noreply.github.com>
2017-05-23 21:01:41 -0500

    Set Up Helper Associations
    Helper Associations for Game, Gamer, and Library are working.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+
+   has_many(:libraries, :class_name => "Library", :foreign_key => "game_id", :dependent => :destroy)
+   has_many(:gamers, :through => :libraries)
 end
