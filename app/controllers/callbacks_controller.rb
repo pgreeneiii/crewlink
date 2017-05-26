caa3aeacc68509e17286673a029931ef9c368e35
Phil Greene <pgreeneiii@users.noreply.github.com>
2017-05-25 20:42:46 -0500

    Devise + Omniauth integration is close
    but not quite working yet. Currently doesn't properly authenticate with
    correct Steam credentials.

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,27 @@
+class CallbacksController < Devise::OmniauthCallbacksController
+   skip_before_action :verify_authenticity_token
+
+   # def auth_callback
+   #    auth = request.env['omniauth.auth']
+   #    session[:current_user] = { nickname: auth[:info][:nickname], image: auth[:info][:image], uid: auth[:uid], name: auth[:info][:name] }
+   #
+   #    @gamer = Gamer.new
+   #    @steam_username = session[:current_user][:nickname]
+   #
+   #    p "****************************************"
+   #    p session[:current_user]
+   #    p @steam_username
+   #    p "****************************************"
+   #
+   # end
+
+   def steam
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+
+      p "****************************************"
+      p @user
+      p "****************************************"
+
+      sign_in_and_redirect @user
+   end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+   def self.from_omniauth
+      where(steam_uid: auth.uid).first_or_create do |user|
+         user.username = auth.info.nickname
+         user.password = Devise.friendly_token[0,20]
+      end
+   end
 end
