377a6b13852ed73213477b418eaa3f36a4dd00e4
Phil Greene <pgreeneiii@users.noreply.github.com>
2017-05-27 10:04:41 -0500

    Cleaning up a few obsolete files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:omniauth_callbacks => "callbacks"}
   get 'gamers/index'
   root to: 'gamers#index'
-  post 'auth/steam/callback' => 'gamers#auth_callback'
 
   # Routes for the Library resource:
   # CREATE
